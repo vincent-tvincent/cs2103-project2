a102e31b0ee88bbca2f908d42d1d7986b501ad4a
Fish1naTank <nicolasfish1224@gmail.com>
2021-11-09 16:59:30 -0500

    Hashmap

--- LRUCache.java
+++ LRUCache.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
  * eviction policy.
  */
 public class LRUCache<T, U> implements Cache<T, U> {
+
+	HashMap<T, U> map;
+	int numOfMisses;
+
 	/**
 	 * @param provider the data provider to consult for a cache miss
 	 * @param capacity the exact number of (key,value) pairs to store in the cache
@@ -13,6 +17,10 @@ public class LRUCache<T, U> implements Cache<T, U> {
 		if (capacity < 1) {
 			throw new IllegalArgumentException("capacity must be at least 1");
 		}
+
+		// init
+		map = new HashMap<T, U>();
+		numOfMisses = 0;
 	}
 
 	/**
@@ -21,7 +29,16 @@ public class LRUCache<T, U> implements Cache<T, U> {
 	 * @return the value associated with the key
 	 */
 	public U get (T key) {
-		return null;  // TODO: implement me
+		if(!isInCache(key)) {
+			// call data provider
+			U value = null; // TODO:
+			// store key
+			map.put(key, value);
+
+			numOfMisses++;
+		}
+		return map.get(key);
+//		return null;  // TODO: implement me
 	}
 
 	/**
@@ -29,15 +46,17 @@ public class LRUCache<T, U> implements Cache<T, U> {
 	 * @return the number of cache misses since the object's instantiation.
 	 */
 	public int getNumMisses () {
-		return 0;  // TODO: implement me
+		return numOfMisses;
+//		return 0;  // TODO: implement me
 	}
 
 	/**
 	 * Returns whether the object with the specified key is contained in the cache.
-	 * @param the key of the object
+	 * @param key of the object
 	 * @return whether the object is contained in the cache.
 	 */
 	public boolean isInCache (T key) {
-		return false;  // TODO: implement me
+		return map.containsKey(key);
+//		return false;  // TODO: implement me
 	}
 }

d0aa38c09bfd0599d33da8254501319ffb5c0dfe
blue_lobster <42709757+vincent-tvincent@users.noreply.github.com>
2021-11-12 18:49:57 -0500

    linklist halfly finished

---
+++ linkedList.java
@@ -0,0 +1,28 @@
+public class linkedList<T> {
+    private class node<T>{
+
+        public node<T> previous, next;
+        public T data;
+
+        public node (T data, node<T> previous, node<T> next){
+            this.data = data;
+            this.previous = previous;
+            this.next = next;
+        }
+
+        public node (T data){
+           this.data = data;
+            previous = null;
+            next = null;
+        }
+    }
+    private node<T> head, tail;
+
+    public linkedList (){
+
+    }
+
+    public node<T> addFirst(T data){return null;}
+    public T getValue(T node){return null;}
+}
+

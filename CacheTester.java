bb2742dc0495d63a80c8c937e7e8dc9687646a1b
Fish1naTank <nicolasfish1224@gmail.com>
2021-11-15 18:20:15 -0500

    moved DataStringProvider into CacheTester

--- CacheTester.java
+++ CacheTester.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.*;
+import java.util.HashMap;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -7,8 +8,37 @@ import static org.junit.jupiter.api.Assertions.*;
  * try to access any methods or variables that are specific to your own LRUCache!
  */
 public class CacheTester {
-	DataStringProvider provider;
-	Cache<Integer, String> cache;
+	private DataStringProvider provider;
+	private Cache<Integer, String> cache;
+
+	private class DataStringProvider implements DataProvider<Integer, String> {
+
+		private HashMap map;
+
+		public DataStringProvider() {
+			initMap();
+		}
+
+		@Override
+		public String get(Integer key) {
+			return (String) map.get(key);
+		}
+
+		private void initMap() {
+			map = new HashMap<Integer, String>();
+			map.put(1, "ONE");
+			map.put(2, "TWO");
+			map.put(3, "THREE");
+			map.put(4, "FOUR");
+			map.put(5, "FIVE");
+			map.put(6, "SIX");
+			map.put(7, "SEVEN");
+			map.put(8, "EIGHT");
+			map.put(9, "NINE");
+			map.put(10, "TEN");
+			map.put(11, "ELEVEN");
+		}
+	}
 
 	@Test
 	public void getItemNotInCache() {
